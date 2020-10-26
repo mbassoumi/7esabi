4ec7fc25f58e99f0e3f88b2ab43d6a85e66f515d
Jehad Bisoomi <jbesoomi@gmail.com>
2020-10-25 22:03:40 -0700

    logout button loading

--- src/components/shared/userSessionMenu.tsx
+++ src/components/shared/userSessionMenu.tsx
@@ -6,14 +6,17 @@ import { GQL_LOGOUT } from '../../graphql/gql/auth/logout';
 import { GqlLogout } from '../../graphql/gql/auth/types/GqlLogout';
 import { showGenericOperationFailedMessage } from '../../graphql/utils/errorsHelper';
 import { useCurrentUser } from '../helpers/storeHelper';
+import './styles/userSessionMenu.scss';
 
 interface UserSessionMenuState {
   popoverVisible: boolean;
+  logoutButtonLoading: boolean;
 }
 
 const UserSessionMenu = () => {
   const [state, setState] = useState({
     popoverVisible: false,
+    logoutButtonLoading: false,
   } as UserSessionMenuState);
 
   const { t, i18n } = useTranslation();
@@ -21,18 +24,28 @@ const UserSessionMenu = () => {
 
   const [logoutFn, { client }] = useMutation<GqlLogout>(GQL_LOGOUT);
 
-  const onPopoverVisibleChage = (newValue: boolean) => {
+  const onPopoverVisibleChange = (newValue: boolean) => {
     setState((state) => ({ ...state, popoverVisible: newValue }));
+  };
+
+  const setLogoutButtonLoading = (loading: boolean) => {
+    setState((state) => ({
+      ...state,
+      logoutButtonLoading: loading,
+    }));
   };
 
   const logout = async () => {
     console.log('logged out');
+
+    setLogoutButtonLoading(true);
 
     // logout backend
     try {
       await logoutFn();
     } catch (e) {
       showGenericOperationFailedMessage(e, t);
+      setLogoutButtonLoading(false);
       return;
     }
 
@@ -44,7 +57,11 @@ const UserSessionMenu = () => {
 
   const popoverContent = (
     <div>
-      <Button onClick={logout} type={'primary'}>
+      <Button
+        loading={state.logoutButtonLoading}
+        onClick={logout}
+        type={'primary'}
+      >
         {t('generic.links.logout')}
       </Button>
     </div>
@@ -60,7 +77,7 @@ const UserSessionMenu = () => {
       trigger="click"
       placement={popoverPlacement}
       visible={state.popoverVisible}
-      onVisibleChange={onPopoverVisibleChage}
+      onVisibleChange={onPopoverVisibleChange}
       className="user-session-menu"
     >
       <Avatar className="user-session-menu__user-icon">
