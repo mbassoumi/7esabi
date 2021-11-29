794b9634d0e756a338ac8061c75ef7f95bc3d86a
jbesoomi <jbesoomi@gmail.com>
2021-11-28 18:40:24 -0800

    fix env vars to run in docker

--- envVars/prod.ts
+++ envVars/prod.ts
@@ -1,4 +1,4 @@
-export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_PROD = true;
 export const API_SERVER =
-  process.env.API_SERVER || "https://mybank-api.bisoomi.com";
+  process.env.API_SERVER || "https://mybank-api.bisoomi.com/api";
 export const DEFAULT_OAUTH_URL = `${API_SERVER}/users/auth/cognito`;
