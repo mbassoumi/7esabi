2efe92b25bc6f070bbaf150bc8caf4f13c6f9393
jbesoomi <jbesoomi@gmail.com>
2021-11-28 20:47:52 -0800

    fix api server url

--- envVars/prod.ts
+++ envVars/prod.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 export const IS_PROD = true;
 export const API_SERVER =
-  process.env.API_SERVER || "https://mybank-api.bisoomi.com/api";
+  process.env.API_SERVER || "https://internal-bank-api.bisoomi.com/api";
 export const DEFAULT_OAUTH_URL = `${API_SERVER}/users/auth/cognito`;
